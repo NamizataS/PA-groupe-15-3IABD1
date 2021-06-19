eca7973a093d0c64a414d8e9c3ceb7061e1dca05
NamizataS <nsangare@icloud.com>
2021-06-19 17:25:53 +0200

    commit to checkout

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/result")
 def resultPage():
--- test.py
+++ test.py
@@ -9,64 +9,69 @@ def toList(arr):
 
 if __name__ == "__main__":
     my_lib = cdll.LoadLibrary(path_to_shared_library)
+    # Dataset
     X = np.array([
-        [1],
-        [2]
+        [1, 1],
+        [2, 3],
+        [3, 3]
     ])
     Y = np.array([
-        2,
-        3
+        1,
+        -1,
+        -1
     ])
 
+    d = [2, 1]
+
+    # Convert array to list
     dataset_inputs = toList(X)
     dataset_expected_outputs = Y.tolist()
-
-    my_lib.create_model.argtypes = [c_int]
-    my_lib.create_model.restype = POINTER(c_float)
-
-    arr_size = 1
-    native_arr = my_lib.create_model(arr_size)
-
-    arr_size += 1
-
     inputs_len = len(dataset_inputs)
     expected_len = len(dataset_expected_outputs)
-    arr_type = type(native_arr)
-    inputs_type = c_float * inputs_len
-    outputs_type = c_float * expected_len
 
-    # Declaration of argtypes and restypes for the function predict_linear_model_classification
-    coordinate_type = c_float * 1
-    my_lib.predict_linear_model_regression.argtypes = [arr_type, coordinate_type, c_int, c_int]
-    my_lib.predict_linear_model_regression.restype = c_float
+    # Declaration of argtypes and restypes for the function create_mlp_model
+    arr_size = len(d)
+    arr_type = c_int * arr_size
+    native_arr = arr_type(*d)
+    my_lib.create_mlp_model.argtypes = [arr_type, c_int]
+    my_lib.create_mlp_model.restype = c_void_p
 
-    plt.axis([-10, 10, -10, 10])
-    test_dataset = [[i] for i in range(-10, 11)]
-    predicted_outputs = [
-        my_lib.predict_linear_model_regression(native_arr, coordinate_type(*point), arr_size, len(point)) for point in
-        test_dataset]
+    model = my_lib.create_mlp_model(native_arr, 2)
+    test_dataset = [[x1 / 10, x2 / 10] for x1 in range(-10, 40, 2) for x2 in range(-10, 40, 2)]
+    colors = ["blue" if output >= 0 else "red" for output in dataset_expected_outputs]
 
-    plt.scatter(test_dataset, predicted_outputs)
-    plt.scatter(dataset_inputs, dataset_expected_outputs, c="purple")
+    # Declaration of argtypes and restypes for the function predict_mlp_model_classification
+    p_type = c_float * 2
+    my_lib.predict_mlp_model_classification.argtypes = [c_void_p, p_type, c_int]
+    my_lib.predict_mlp_model_classification.restype = POINTER(c_float)
+    p_len = 2
+
+    predicted_outputs = [my_lib.predict_mlp_model_classification(model, p_type(*p), p_len)[0] for p in test_dataset]
+    predicted_outputs_colors = ['blue' if label >= 0 else 'red' for label in predicted_outputs]
+    plt.scatter([p[0] for p in test_dataset], [p[1] for p in test_dataset], c=predicted_outputs_colors)
+    plt.scatter([p[0] for p in X], [p[1] for p in X], c=colors, s=200)
     plt.show()
 
-    my_lib.train_regression_linear_model.argtypes = [arr_type, inputs_type, outputs_type, c_int, c_int, c_int]
-    my_lib.train_regression_linear_model.restype = None
+    flattened_len = len(dataset_inputs)
+    # Declaration of argtypes and restypes for the function predict_mlp_model_classification
+    inputs_type = c_float * flattened_len
+    outputs_type = c_float * expected_len
+    my_lib.train_classification_stochastic_backdrop_mlp_model.argtypes = [c_void_p, inputs_type, outputs_type, c_float,
+                                                                          c_int, c_int, c_int]
+    my_lib.train_classification_stochastic_backdrop_mlp_model.restype = None
     inputs_native = inputs_type(*dataset_inputs)
     outputs_native = outputs_type(*dataset_expected_outputs)
 
-    my_lib.train_regression_linear_model(native_arr, inputs_native, outputs_native, arr_size, inputs_len, expected_len)
-    np_arr = np.ctypeslib.as_array(native_arr, (arr_size,))
+    my_lib.train_classification_stochastic_backdrop_mlp_model(model, inputs_native, outputs_native, 0.001, 100000,
+                                                              flattened_len, expected_len)
 
-    plt.axis([-10, 10, -10, 10])
-    predicted_outputs = [
-        my_lib.predict_linear_model_regression(native_arr, coordinate_type(*point), arr_size, len(point)) for point in
-        test_dataset]
-
-    plt.scatter(test_dataset, predicted_outputs)
-    plt.scatter(dataset_inputs, dataset_expected_outputs, c="purple")
+    predicted_outputs = [my_lib.predict_mlp_model_classification(model, p_type(*p), p_len)[0] for p in test_dataset]
+    predicted_outputs_colors = ['blue' if label >= 0 else 'red' for label in predicted_outputs]
+    plt.scatter([p[0] for p in test_dataset], [p[1] for p in test_dataset], c=predicted_outputs_colors)
+    plt.scatter([p[0] for p in X], [p[1] for p in X], c=colors, s=200)
     plt.show()
 
-    my_lib.destroy_array.argtypes = [POINTER(c_float), c_int]
-    my_lib.destroy_array.restype = None
-    my_lib.destroy_array(native_arr, arr_size)
+    # Free memory
+    my_lib.destroy_model.argtypes = [c_void_p]
+    my_lib.destroy_model.restype = None
+    my_lib.destroy_model(model)
