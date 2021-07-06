86efb03173240b46765008dd287c2b22652126d8
NamizataS <nsangare@icloud.com>
2021-07-06 21:29:09 +0200

    rbf works now

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ pub struct MLP{
 pub struct RBF{
     W: Vec<f32>,
     mu: Vec<f32>,
-    gamma: Vec<f32>,
+    gamma: f32,//Vec<f32>,
     K: i32,
     sample_count: usize,
     input_dim: usize
@@ -336,7 +336,7 @@ pub extern "C" fn predict_mlp_model_regression(model: *mut MLP, sample_inputs: *
 pub extern "C" fn create_rbf_model(K: i32, dataset_inputs: *mut f32, dataset_inputs_len: i32, input_dim: i32) -> *mut RBF{
     let mut rng = rand::thread_rng();
     let mut W:Vec<f32> = Vec::new();
-    let mut gamma:Vec<f32> = Vec::new();
+    let mut gamma = rng.gen_range(-1.0..1.0);;
     let mut mu:Vec<f32> = Vec::new();
     let dataset_inputs = unsafe{
         from_raw_parts(dataset_inputs,dataset_inputs_len as usize)
@@ -362,10 +362,10 @@ pub extern "C" fn create_rbf_model(K: i32, dataset_inputs: *mut f32, dataset_inp
         }
     }
 
-    for _ in 0..K{
-        let mut num = rng.gen_range(0.0..1.0);
+    /*for _ in 0..K{
+        let mut num = rng.gen_range(-1.0..1.0);
         gamma.push(num);
-    }
+    }*/
 
     let model = RBF{
         W,
@@ -378,9 +378,152 @@ pub extern "C" fn create_rbf_model(K: i32, dataset_inputs: *mut f32, dataset_inp
     let model_leaked = Box::leak(Box::from(model));
     model_leaked as *mut RBF
 }
+#[no_mangle]
+pub extern "C" fn has_converged(old_mu: Vec<f32>, new_mu: Vec<f32>)->bool{
+    old_mu.iter().eq(new_mu.iter())
+}
+#[no_mangle]
+pub extern "C" fn evaluate_centers(model: &mut RBF, cluster: Vec<f32>)->Vec<f32>{
+    let mut cluster_size = cluster.len() / model.input_dim;
+    let mut new_center = Vec::with_capacity(model.input_dim);
+    for _ in 0..model.input_dim{
+        new_center.push(0.0f32);
+    }
+    for i in (0..cluster.len()).step_by(model.input_dim){
+        let mut sample_cluster = &cluster[i..(i+model.input_dim)];
+        for j in 0..model.input_dim{
+            new_center[j] += sample_cluster[j];
+        }
+    }
+    for i in 0..new_center.len(){
+        new_center[i] = new_center[i] / (cluster_size as f32);
+    }
+    new_center
+}
+#[no_mangle]
+pub extern "C" fn evaluate_clusters(model: &mut RBF, flattened_dataset_inputs: &[f32])->Vec<Vec<f32>>{
+    let mut clusters = Vec::new();
+    for _ in 0..model.K{
+        let mut cluster = Vec::new();
+        clusters.push(cluster);
+    }
+    for i in 0..model.sample_count{
+        let mut sample_inputs = &flattened_dataset_inputs[i*model.input_dim..(i+1)*model.input_dim];
+        let mut distances = Vec::new();
+        for j in 0..(model.K as usize){
+            let mut sample_mu = &model.mu[j*model.input_dim..(j+1)*model.input_dim];
+            let mut distance = euclidean_distance(sample_inputs, sample_mu);
+            distances.push(distance);
+        }
+        let mut min = distances[0];
+        let mut min_pos:usize = 0;
+        for j in 1..distances.len(){
+            if distances[j] < min {
+                min = distances[j];
+                min_pos = j;
+            }
+        }
+        for j in 0..model.input_dim{
+            clusters[min_pos].push(sample_inputs[j]);
+        }
+    }
+    clusters
+}
+#[no_mangle]
+pub extern "C" fn lloyd2(model: *mut RBF, flattened_dataset_inputs: *const f32, inputs_len: i32, iterations: i32){
+    let mut model = unsafe{
+        model.as_mut().unwrap()
+    };
+    let mut flattened_dataset_inputs = unsafe{
+        from_raw_parts(flattened_dataset_inputs, inputs_len as usize)
+    };
+    let mut clusters:Vec<Vec<f32>>;
+    for _ in 0..iterations{
+        clusters = evaluate_clusters(model, flattened_dataset_inputs);
+        let mut mu_count:usize = 0;
+        for i in 0..(model.K as usize){
+            let mut cluster = clusters[i].to_vec();
+            let mut new_center = evaluate_centers(model, cluster);
+            for j in 0..model.input_dim{
+                model.mu[mu_count] = new_center[j];
+                mu_count += 1;
+            }
+        }
+    }
+}
+#[no_mangle]
+pub extern "C" fn lloyd(model: *mut RBF, flattened_dataset_inputs: *const f32, inputs_len: i32, iterations: i32 ) {
+    let mut model = unsafe{
+        model.as_mut().unwrap()
+    };
+    let flattened_dataset_inputs = unsafe{
+        from_raw_parts(flattened_dataset_inputs, inputs_len as usize)
+    };
+
+    for _ in 0..iterations {
+        let mut clusters:Vec<Vec<f32>> = Vec::new();
+        for i in 0..(model.K as usize){
+            let mut cluster = Vec::new();
+            clusters.push(cluster);
+        }
+
+        for i in (0..(inputs_len as usize)).step_by(model.input_dim) {
+            let mut distance_points = Vec::with_capacity(model.mu.len());
+            let mut sample_inputs = &flattened_dataset_inputs[i..i + (model.input_dim)];
+            let mut point_coord = Vec::new();
+
+            for j in (0..model.mu.len()).step_by(model.input_dim) {
+                let mut sample_mu = &model.mu[j..j + model.input_dim];
+                let mut distance = 0.0f32;
+                point_coord = Vec::new();
+
+                for k in 0..model.input_dim {
+                    distance += powf((sample_inputs[k] - sample_mu[k]), 2.0);
+                    point_coord.push(sample_inputs[k]);
+                }
+
+                distance = sqrtf(distance);
+                //distance = powf(distance, 2.0);
+                distance_points.push(distance);
+            }
+            let mut min = [distance_points[0], 0.0];
+            for l in 1..(distance_points.len()) {
+                if distance_points[l] < min[0] {
+                    min[0] = distance_points[l];
+                    min[1] = l as f32;
+                }
+            }
+            for n in 0..point_coord.len(){
+                clusters[(min[1] as usize)].push(point_coord[n]);
+            }
+        }
+
+        let mut m = 0;
+        for i in 0..clusters.len() {
+            let mut point = Vec::with_capacity(model.input_dim);
+            for j in 0..model.input_dim {
+                point.push(0.0f32);
+            }
+            let mut cluster = &clusters[i];
+            for j in 0..cluster.len() {
+                for k in 0..point.len() {
+                    point[k] += cluster[j];
+                }
+            }
+
+            for l in 0..point.len() {
+                point[l] = point[l] / (cluster.len() as f32);
+            }
+            for k in 0..model.input_dim{
+                model.mu[m] = point[k];
+                m+=1;
+            }
+        }
+    }
+}
 
 #[no_mangle]
-pub extern "C" fn train_rbf_model(model: &mut RBF, flattened_dataset_inputs: *mut f32, dataset_outputs: *mut f32, dataset_inputs_len: i32, dataset_output_len: i32, output_dim: i32){
+pub extern "C" fn train_rbf_model_regression(model: &mut RBF, flattened_dataset_inputs: *mut f32, dataset_outputs: *mut f32, dataset_inputs_len: i32, dataset_output_len: i32, output_dim: i32){
     let mut phi:Vec<f32> = Vec::new();
     let flattened_dataset_inputs = unsafe{
         from_raw_parts(flattened_dataset_inputs, dataset_inputs_len as usize)
@@ -399,7 +542,7 @@ pub extern "C" fn train_rbf_model(model: &mut RBF, flattened_dataset_inputs: *mu
             }
             distance = sqrtf(distance);
             distance = powf(distance, 2.0);
-            let mut value = expf(-model.gamma[gamma_count] * distance);
+            let mut value = expf(-model.gamma * distance);
             phi.push(value);
             gamma_count += 1;
         }
@@ -423,14 +566,12 @@ pub extern "C" fn predict_rbf_model_regression(model: *mut RBF, sample_inputs: *
         from_raw_parts(sample_inputs, model.input_dim)
     };
     let mut sum_result = 0.0f32;
+    let mut mu_count = 0 as usize;
     for i in 0..model.W.len(){
-        let mut distance = 0.0f32;
-        for j in 0..model.input_dim{
-            distance += powf(sample_inputs[j] - model.mu[i+j], 2.0);
-        }
-        distance = sqrtf(distance);
+        let mut sample_mu = &model.mu[i*model.input_dim..(i+1)*model.input_dim];
+        let mut distance = euclidean_distance(sample_inputs, sample_mu);
         distance = powf(distance, 2.0);
-        sum_result += model.W[i] * expf(-model.gamma[i] *distance );
+        sum_result += model.W[i] * expf(-model.gamma *distance );
     }
     sum_result
 }
@@ -447,37 +588,54 @@ pub extern "C" fn predict_rbf_model_classification(model: *mut RBF, sample_input
     rslt
 }
 #[no_mangle]
-pub extern "C" fn train_em_rbf_model_classification(model: *mut RBF, flattened_dataset_inputs: *mut f32, dataset_outputs: *mut f32, dataset_inputs_len: i32, dataset_outputs_len: i32, output_dim: i32, learning_rate: f32, iterations: i32){
+pub extern "C" fn train_rbf_model_classification(model: *mut RBF, flattened_dataset_inputs: *mut f32, dataset_outputs: *mut f32, dataset_inputs_len: i32, dataset_outputs_len: i32, iterations: i32, learning_rate: f32){
     let mut model = unsafe{
         model.as_mut().unwrap()
     };
-    train_em_rbf_model(model, flattened_dataset_inputs, dataset_outputs, dataset_inputs_len, dataset_outputs_len,output_dim, learning_rate, iterations, true);
-}
-#[no_mangle]
-pub extern "C" fn train_em_rbf_model_regression(model: *mut RBF, flattened_dataset_inputs: *mut f32, dataset_outputs: *mut f32, dataset_inputs_len: i32, dataset_outputs_len: i32, output_dim: i32, learning_rate: f32, iterations: i32){
-    let mut model = unsafe{
-        model.as_mut().unwrap()
+    let mut flattened_dataset_inputs = unsafe{
+        from_raw_parts_mut(flattened_dataset_inputs, dataset_inputs_len as usize)
     };
-    train_em_rbf_model(model, flattened_dataset_inputs, dataset_outputs, dataset_inputs_len, dataset_outputs_len,output_dim, learning_rate, iterations, false);
-}
-#[no_mangle]
-pub extern "C" fn train_em_rbf_model(model: &mut RBF, flattened_dataset_inputs: *mut f32, dataset_outputs: *mut f32, dataset_inputs_len: i32, dataset_outputs_len: i32, output_dim: i32, learning_rate: f32, iterations: i32, is_classification: bool){
-    for i in 0..(model.K as usize){
-        let precision = 0.000001;
-        let mut previous_step_rate = 1.0f32;
-        let mut iterations_count = 0;
-        while previous_step_rate > precision && iterations_count < iterations{
-            train_rbf_model(model, flattened_dataset_inputs, dataset_outputs, dataset_inputs_len, dataset_outputs_len, output_dim);
-            let mut prev_gamma = model.gamma[i];
-            model.gamma[i] -= learning_rate * gradient(model, i, flattened_dataset_inputs, dataset_inputs_len, dataset_outputs, dataset_outputs_len, is_classification);
-            previous_step_rate = model.gamma[i] - prev_gamma;
-            iterations_count += 1;
+    let mut dataset_outputs = unsafe{
+        from_raw_parts_mut(dataset_outputs, dataset_outputs_len as usize)
+    };
+    println!("mu are {:?}", model.mu);
+    let mut phi:Vec<f32> = Vec::new();
+    for i in 0..model.sample_count{
+        let mut sample_inputs = &flattened_dataset_inputs[i*model.input_dim..(i+1)*model.input_dim];
+        for j in 0..(model.K as usize){
+            let mut sample_mu = &model.mu[j*model.input_dim..(j+1)*model.input_dim];
+            phi.push(expf(-model.gamma * powf(euclidean_distance(sample_inputs,sample_mu), 2.0)));
         }
     }
+    let mut rng = rand::thread_rng();
+    let mut k:usize = 0;
+    let mut X;
+    let mut Xk;
+    let mut yk = 0.0f32;
+    let mut gXk = 0.0f32;
+    for _ in 0..iterations{
+        k = rng.gen_range(0..model.sample_count);
+        X = &flattened_dataset_inputs[k*model.input_dim..(k+1)*model.input_dim];
+        Xk = &phi[k*(model.K as usize)..(k+1)*(model.K as usize)];
+        yk = dataset_outputs[k];
+        gXk = predict_rbf_model_classification(model, X.as_ptr());
+        for i in 0..model.W.len(){
+            model.W[i] += learning_rate * (yk - gXk) * Xk[i];
+        }
+    }
+    model.gamma -= learning_rate * gradient(model, flattened_dataset_inputs.as_mut_ptr(), dataset_inputs_len, dataset_outputs.as_mut_ptr(), dataset_outputs_len, true);
+}
+#[no_mangle]
+pub extern "C" fn train_em_rbf_model_regression(model: *mut RBF, flattened_dataset_inputs: *mut f32, dataset_outputs: *mut f32, dataset_inputs_len: i32, dataset_outputs_len: i32, output_dim: i32, learning_rate: f32){
+    let mut model = unsafe{
+        model.as_mut().unwrap()
+    };
+    train_rbf_model_regression(model, flattened_dataset_inputs, dataset_outputs, dataset_inputs_len, dataset_outputs_len, output_dim);
+    model.gamma -= learning_rate * gradient(model, flattened_dataset_inputs, dataset_inputs_len, dataset_outputs, dataset_outputs_len, false);
 }
 
 #[no_mangle]
-pub extern "C" fn gradient(model: &mut RBF, i: usize, flattened_dataset_inputs: *mut f32, inputs_len: i32, dataset_outputs: *mut f32, outputs_len: i32,is_classification: bool)->f32{
+pub extern "C" fn gradient(model: &mut RBF, flattened_dataset_inputs: *mut f32, inputs_len: i32, dataset_outputs: *mut f32, outputs_len: i32,is_classification: bool)->f32{
     let mut flattened_dataset_inputs = unsafe{
         from_raw_parts(flattened_dataset_inputs, inputs_len as usize)
     };
@@ -485,22 +643,35 @@ pub extern "C" fn gradient(model: &mut RBF, i: usize, flattened_dataset_inputs: 
         from_raw_parts(dataset_outputs, outputs_len as usize)
     };
     let mut sum_result = 0.0f32;
-    for j in 0..model.sample_count{
-        let mut sample_inputs = &flattened_dataset_inputs[j..(j+model.input_dim)];
-        let mut sample_mu = &model.mu[i..(i+model.input_dim)];
-        let mut distance = 0.0f32;
-        for k in 0..model.input_dim{
-            distance += powf(sample_inputs[k] - sample_mu[k], 2.0);
-        }
-        distance = sqrtf(distance);
+    let mut output_count = 0 as usize;
+    for i in (0..(inputs_len as usize)).step_by(model.input_dim){
+        let mut sample_inputs = &flattened_dataset_inputs[i..(i+model.input_dim)];
         let mut pred = 0.0f32;
         if is_classification{
             pred = predict_rbf_model_classification(model, sample_inputs.as_ptr());
-        } else {
+        }else{
             pred = predict_rbf_model_regression(model, sample_inputs.as_ptr());
         }
-        sum_result += 2.0 * distance * ( dataset_outputs[j] - pred ) * pred;
+        let mut first_derivative = 0.0f32;
+        for j in (0..model.mu.len()).step_by(model.input_dim){
+            let mut sample_mu = &model.mu[j..(j+model.input_dim)];
+            let mut distance = euclidean_distance(sample_inputs, sample_mu);
+            distance = powf(distance, 2.0);
+            first_derivative += distance * pred;
+        }
+        let mut second_derivative = -2.0 * (dataset_outputs[output_count] - pred);
+        output_count += 1;
+        sum_result += second_derivative * first_derivative;
     }
+    sum_result/(model.sample_count as f32)
+}
+
+fn euclidean_distance(sample_inputs: &[f32], sample_mu: &[f32])->f32{
+    let mut sum_result = 0.0f32;
+    for i in 0..sample_inputs.len(){
+        sum_result += powf(sample_inputs[i] - sample_mu[i], 2.0);
+    }
+    sum_result = sqrtf(sum_result);
     sum_result
 }
 
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from ctypes import *
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from mpl_toolkits.mplot3d import Axes3D
 
 path_to_shared_library = "target/debug/librust_lib.dylib"
 def toList(arr):
@@ -9,7 +10,6 @@ def toList(arr):
 
 if __name__ == "__main__":
     my_lib = cdll.LoadLibrary(path_to_shared_library)
-    # Dataset
     X = np.array([
         [1, 1],
         [2, 3],
@@ -21,57 +21,57 @@ if __name__ == "__main__":
         -1
     ])
 
-    d = [2, 1]
-
     # Convert array to list
     dataset_inputs = toList(X)
     dataset_expected_outputs = Y.tolist()
     inputs_len = len(dataset_inputs)
-    expected_len = len(dataset_expected_outputs)
+    outputs_len = len(dataset_expected_outputs)
+    output_dim = 1
 
-    # Declaration of argtypes and restypes for the function create_mlp_model
-    arr_size = len(d)
-    arr_type = c_int * arr_size
-    native_arr = arr_type(*d)
-    my_lib.create_mlp_model.argtypes = [arr_type, c_int]
-    my_lib.create_mlp_model.restype = c_void_p
+    dataset_inputs_type = c_float * inputs_len
+    dataset_inputs_native = dataset_inputs_type(*dataset_inputs)
+    dataset_outputs_type = c_float * outputs_len
+    dataset_outputs_native = dataset_outputs_type(*dataset_expected_outputs)
+    sample_inputs_type = c_float * len(X[0])
 
-    model = my_lib.create_mlp_model(native_arr, 2)
+    # create model
+    my_lib.create_rbf_model.argtypes = [c_int, dataset_inputs_type, c_int, c_int]
+    my_lib.create_rbf_model.restype = c_void_p
+
+    model = my_lib.create_rbf_model(3, dataset_inputs_native, inputs_len, len(X[0]))
+
+    # test dataset
     test_dataset = [[x1 / 10, x2 / 10] for x1 in range(-10, 40, 2) for x2 in range(-10, 40, 2)]
-    colors = ["blue" if output >= 0 else "red" for output in dataset_expected_outputs]
+    colors = ["blue" if output >= 0 else "red" for output in dataset_expected_outputs]  # destroy model
 
-    # Declaration of argtypes and restypes for the function predict_mlp_model_classification
-    p_type = c_float * 2
-    my_lib.predict_mlp_model_classification.argtypes = [c_void_p, p_type, c_int]
-    my_lib.predict_mlp_model_classification.restype = POINTER(c_float)
-    p_len = 2
-
-    predicted_outputs = [my_lib.predict_mlp_model_classification(model, p_type(*p), p_len)[0] for p in test_dataset]
+    # before training
+    my_lib.predict_rbf_model_classification.argtypes = [c_void_p, sample_inputs_type]
+    my_lib.predict_rbf_model_classification.restype = c_float
+    predicted_outputs = [my_lib.predict_rbf_model_classification(model, sample_inputs_type(*p)) for p in test_dataset]
     predicted_outputs_colors = ['blue' if label >= 0 else 'red' for label in predicted_outputs]
     plt.scatter([p[0] for p in test_dataset], [p[1] for p in test_dataset], c=predicted_outputs_colors)
     plt.scatter([p[0] for p in X], [p[1] for p in X], c=colors, s=200)
     plt.show()
 
-    flattened_len = len(dataset_inputs)
-    # Declaration of argtypes and restypes for the function predict_mlp_model_classification
-    inputs_type = c_float * flattened_len
-    outputs_type = c_float * expected_len
-    my_lib.train_classification_stochastic_backdrop_mlp_model.argtypes = [c_void_p, inputs_type, outputs_type, c_float,
-                                                                          c_int, c_int, c_int]
-    my_lib.train_classification_stochastic_backdrop_mlp_model.restype = None
-    inputs_native = inputs_type(*dataset_inputs)
-    outputs_native = outputs_type(*dataset_expected_outputs)
+    # training
+    my_lib.lloyd2.argtypes = [c_void_p, dataset_inputs_type, c_int]
+    my_lib.lloyd2.restype = None
+    my_lib.lloyd2(model, dataset_inputs_native, inputs_len)
 
-    my_lib.train_classification_stochastic_backdrop_mlp_model(model, inputs_native, outputs_native, 0.001, 100000,
-                                                              flattened_len, expected_len)
+    my_lib.train_rbf_model_classification.argtypes = [c_void_p, dataset_inputs_type, dataset_outputs_type, c_int, c_int,
+                                                      c_int, c_float]
+    my_lib.train_rbf_model_classification.restype = None
+    my_lib.train_rbf_model_classification(model, dataset_inputs_native, dataset_outputs_native, inputs_len, outputs_len,
+                                          100000, 0.0001)
 
-    predicted_outputs = [my_lib.predict_mlp_model_classification(model, p_type(*p), p_len)[0] for p in test_dataset]
+    # after training
+    predicted_outputs = [my_lib.predict_rbf_model_classification(model, sample_inputs_type(*p)) for p in test_dataset]
     predicted_outputs_colors = ['blue' if label >= 0 else 'red' for label in predicted_outputs]
     plt.scatter([p[0] for p in test_dataset], [p[1] for p in test_dataset], c=predicted_outputs_colors)
     plt.scatter([p[0] for p in X], [p[1] for p in X], c=colors, s=200)
     plt.show()
 
-    # Free memory
-    my_lib.destroy_model.argtypes = [c_void_p]
-    my_lib.destroy_model.restype = None
-    my_lib.destroy_model(model)
+    # destroy model
+    my_lib.destroy_rbf_model.argtypes = [c_void_p]
+    my_lib.destroy_rbf_model.restype = None
+    my_lib.destroy_rbf_model(model)
