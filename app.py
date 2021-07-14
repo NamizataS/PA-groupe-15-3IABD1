5617b27498462a8087e2405271e407faf3301c78
StrockBecca <strockbecca@gmail.com>
2021-07-14 17:00:14 +0200

    add begining upload images

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -10,6 +11,13 @@ def home():
 def resultPage():
     return render_template("result.html")
 
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return render_template("result.html")
 
 if __name__ == "__main__":
     app.run()
