3a0f30d079085388cd2c7a2fef8c960e154ed99c
NamizataS <nsangare@icloud.com>
2021-07-17 21:36:59 +0200

    can use the models on the website

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import predict
 
 app = Flask(__name__)
 
@@ -11,6 +13,24 @@ def home():
 @app.route("/result")
 def resultPage():
     return render_template("result.html")
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        image = request.files['file']
+        image.save(secure_filename(image.filename))
+        image = secure_filename(image.filename)
+        return redirect(url_for("display_results", image=image))
+    else:
+        return render_template("result.html")
+
+
+@app.route('/display_results<image>')
+def display_results(image):
+    predict_obj = predict.Predict(image, (32, 32))
+    prediction = predict_obj.predict_rbf()
+    return render_template("display_results.html", pred=prediction)
 
 
 if __name__ == "__main__":
